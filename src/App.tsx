25aafdc2fc69b48a2d097063ab68d425e61d3a2c
Kuljeet-123 <kuljeetsingh951@gmail.com>
2021-07-21 23:24:19 +0530

    inintiated recipe box

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect, FormEvent } from 'react';
 import './App.css';
 
 function App() {
+  const [recipesFound, setRecipesFound] = useState([]);
+  const [recipeSearch, setRecipeSearch] = useState('');
+
+  const searchForRecipes = async (recipeSearch:string): Promise<any> => {
+    const data = await fetch(`http://localhost:3001/?search=${recipeSearch}`);
+    return (await data.json()).results;
+  }
+
+  
+  const handleSearch = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const form = event.target as HTMLFormElement;
+    const input = form.querySelector('searchText') as HTMLInputElement;
+    setRecipeSearch(input.value);
+  }
+
+  useEffect(() => {
+    (async () => {
+      const query = encodeURIComponent(recipeSearch);
+      if(query) {
+        const recipes = await searchForRecipes(query); 
+        setRecipesFound(recipes);
+      }
+   
+    })();
+  }, [recipeSearch]);
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Recipe Box</h1>
+      <form className="searchForm" onSubmit={event => handleSearch(event)}>
+        <input id="searchText" type="text" />
+        <button id="searchButton" onClick={() => setRecipeSearch(recipeSearch)}>Search</button>
+      </form>
     </div>
   );
 }
